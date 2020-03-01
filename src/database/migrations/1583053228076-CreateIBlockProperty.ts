36cecd6dabc7abaa5f2012300b3f1970ad7cff09
Andrey Mashukov <andre.mashukov@gmail.com>
2020-03-01 17:14:25 +0800

    to lower case

--- src/database/migrations/1583053228076-CreateIBlockProperty.ts
+++ src/database/migrations/1583053228076-CreateIBlockProperty.ts
@@ -3,37 +3,37 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateIBlockProperty1583053228076 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(`
-    CREATE TABLE IF NOT EXISTS b_iblock_property (
-      ID int(11) NOT NULL auto_increment,
-      TIMESTAMP_X timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-      IBLOCK_ID int(11) NOT NULL,
-      NAME varchar(100) NOT NULL,
-      ACTIVE char(1) NOT NULL default 'Y',
-      SORT int(11) NOT NULL default '500',
-      CODE varchar(50) default NULL,
-      DEFAULT_VALUE text,
-      PROPERTY_TYPE char(1) NOT NULL default 'S',
-      ROW_COUNT int(11) NOT NULL default '1',
-      COL_COUNT int(11) NOT NULL default '30',
-      LIST_TYPE char(1) NOT NULL default 'L',
-      MULTIPLE char(1) NOT NULL default 'N',
-      XML_ID varchar(100) default NULL,
-      FILE_TYPE varchar(200) default NULL,
-      MULTIPLE_CNT int(11) default NULL,
-      TMP_ID varchar(40) default NULL,
-      LINK_IBLOCK_ID int(18) default NULL,
-      WITH_DESCRIPTION char(1) default NULL,
-      SEARCHABLE char(1) NOT NULL default 'N',
-      FILTRABLE char(1) NOT NULL default 'N',
-      IS_REQUIRED char(1) default NULL,
-      VERSION int(11) NOT NULL default '1',
-      USER_TYPE varchar(255) default NULL,
-      USER_TYPE_SETTINGS text,
-      HINT varchar(255) default NULL,
-      PRIMARY KEY  (ID),
-      KEY ix_iblock_property_1 (IBLOCK_ID),
-      KEY ix_iblock_property_2 (CODE)
-    ) DEFAULT CHARSET=cp1251 AUTO_INCREMENT=23 ;
+    create table if not exists b_iblock_property (
+      id int(11) not null auto_increment,
+      timestamp_x timestamp not null default current_timestamp on update current_timestamp,
+      iblock_id int(11) not null,
+      name varchar(100) not null,
+      active char(1) not null default 'y',
+      sort int(11) not null default '500',
+      code varchar(50) default null,
+      default_value text,
+      property_type char(1) not null default 's',
+      row_count int(11) not null default '1',
+      col_count int(11) not null default '30',
+      list_type char(1) not null default 'l',
+      multiple char(1) not null default 'n',
+      xml_id varchar(100) default null,
+      file_type varchar(200) default null,
+      multiple_cnt int(11) default null,
+      tmp_id varchar(40) default null,
+      link_iblock_id int(18) default null,
+      with_description char(1) default null,
+      searchable char(1) not null default 'n',
+      filtrable char(1) not null default 'n',
+      is_required char(1) default null,
+      version int(11) not null default '1',
+      user_type varchar(255) default null,
+      user_type_settings text,
+      hint varchar(255) default null,
+      primary key  (id),
+      key ix_iblock_property_1 (iblock_id),
+      key ix_iblock_property_2 (code)
+    ) default charset=cp1251 auto_increment=23 ;
     `);
   }
 
