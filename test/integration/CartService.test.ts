8c576bc4d60f5d2d14e6b4008a3c7cf57208019e
Andrey Mashukov <andre.mashukov@gmail.com>
2020-04-12 17:23:30 +0800

    Added CartService test

--- test/integration/CartService.test.ts
+++ test/integration/CartService.test.ts
@@ -1,12 +1,18 @@
 import { Container } from 'typedi';
 import { Connection } from 'typeorm';
 
-import { Pet } from '../../src/api/models/Pet';
-import { PetService } from '../../src/api/services/PetService';
+import { CartItem } from '../../src/api/models/CartItem';
+import { CartService } from '../../src/api/services/CartService';
 import { closeDatabase, createDatabaseConnection, migrateDatabase } from '../utils/database';
 import { configureLogger } from '../utils/logger';
 
-describe('PetService', () => {
+const PRODUCT = {
+  productId: 1426,
+  blockId: 4,
+  price: 599.00,
+};
+
+describe('CartService', () => {
 
     // -------------------------------------------------------------------------
     // Setup up
@@ -30,22 +36,27 @@ describe('PetService', () => {
     // Test cases
     // -------------------------------------------------------------------------
 
-    test('should create a new pet in the database', async (done) => {
-        const pet = new Pet();
-        pet.id = 'xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx';
-        pet.name = 'test';
-        pet.age = 1;
-        const service = Container.get<PetService>(PetService);
-        const resultCreate = await service.create(pet);
-        expect(resultCreate.name).toBe(pet.name);
-        expect(resultCreate.age).toBe(pet.age);
+    test('should add a new item to the cart', async (done) => {
+        const cartItem = new CartItem();
+        cartItem.id = 1;
+        cartItem.fuserId = 1;
+        cartItem.blockId = PRODUCT.blockId;
+        cartItem.productId = PRODUCT.productId;
+        cartItem.price = PRODUCT.price;
+        cartItem.currency = 'RUB';
+        cartItem.quantity = 1;
+        const service = Container.get<CartService>(CartService);
+        const resultCreate = await service.addCartItem(cartItem);
 
-        const resultFind = await service.findOne(resultCreate.id);
+        expect(resultCreate.productId).toBe(cartItem.productId);
+        expect(resultCreate.quantity).toBe(cartItem.quantity);
+
+        const resultFind = await service.findByFuserIdAndProductId(1, 1426);
         if (resultFind) {
-            expect(resultFind.name).toBe(pet.name);
-            expect(resultFind.age).toBe(pet.age);
+            expect(resultFind.productId).toBe(cartItem.productId);
+            expect(resultFind.quantity).toBe(cartItem.quantity);
         } else {
-            // fail('Could not find pet');
+            fail('Could not find cart item');
         }
         expect(1).toBe(1);
         done();
