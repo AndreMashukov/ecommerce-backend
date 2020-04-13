60f1601ecfa78c97d095ac2e6b17028d184289fc
Andrey Mashukov <andre.mashukov@gmail.com>
2020-04-13 18:30:30 +0800

    e2e test for CartController

---
+++ src/database/seeds/CreateCartItem.ts
@@ -0,0 +1,22 @@
+import { Connection } from 'typeorm';
+import { Factory, Seed } from 'typeorm-seeding';
+
+import { CartItem } from '../../../src/api/models/CartItem';
+
+export class CreateCartItem implements Seed {
+
+    public async seed(factory: Factory, connection: Connection): Promise<CartItem> {
+        const em = connection.createEntityManager();
+
+        const item = new CartItem();
+        item.id = 1;
+        item.fuserId = 1;
+        item.blockId = 4;
+        item.productId = 1426;
+        item.quantity = 1;
+        item.price = 599.0;
+        item.currency = 'RUB';
+        return await em.save(item);
+    }
+
+}
---
+++ test/e2e/api/cart.test.ts
@@ -0,0 +1,49 @@
+import * as nock from 'nock';
+import request from 'supertest';
+import { runSeed } from 'typeorm-seeding';
+import { CartItem } from '../../../src/api/models/CartItem';
+import { CreateCartItem } from '../../../src/database/seeds/CreateCartItem';
+import { closeDatabase } from '../../utils/database';
+import { BootstrapSettings } from '../utils/bootstrap';
+import { prepareServer } from '../utils/server';
+
+describe('/api/cart', () => {
+  let cartItem: CartItem;
+  // let bruceAuthorization: string;
+  let settings: BootstrapSettings;
+
+  // -------------------------------------------------------------------------
+  // Setup up
+  // -------------------------------------------------------------------------
+
+  beforeAll(async () => {
+    settings = await prepareServer({ migrate: true });
+    cartItem = await runSeed<CartItem>(CreateCartItem);
+    // bruceAuthorization = Buffer.from('bruce:1234').toString('base64');
+  });
+
+  // -------------------------------------------------------------------------
+  // Tear down
+  // -------------------------------------------------------------------------
+
+  afterAll(async () => {
+    nock.cleanAll();
+    await closeDatabase(settings.connection);
+  });
+
+  // -------------------------------------------------------------------------
+  // Test cases
+  // -------------------------------------------------------------------------
+
+  test('GET: / should return a list of cart items', async done => {
+    const response = await request(settings.app)
+      .get('/api/cart/?fuserId=1')
+      // .set('Authorization', `Basic ${bruceAuthorization}`)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    console.log(response.body);
+    console.log(cartItem);
+    expect(response.body.length).toBe(1);
+    done();
+  });
+});
