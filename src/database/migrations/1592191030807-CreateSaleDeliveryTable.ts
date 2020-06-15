ac3c4146ead5b506584ef5b2dfa3a7e466e82d31
Andrey Mashukov <andre.mashukov@gmail.com>
2020-06-15 12:06:05 +0800

    b_sale_delivery table created

---
+++ src/database/migrations/1592191030807-CreateSaleDeliveryTable.ts
@@ -0,0 +1,159 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey
+} from 'typeorm';
+
+export class CreateSaleDeliveryTable1592191030807
+  implements MigrationInterface {
+  public saleDeliveryTable = new Table({
+    name: 'b_sale_delivery',
+    columns: [
+      {
+        name: 'id',
+        type: 'int',
+        length: '18',
+        isPrimary: true,
+        isNullable: false,
+        isGenerated: true
+      },
+      {
+        name: 'name',
+        type: 'varchar',
+        length: '255',
+        isNullable: false
+      },
+      {
+        name: 'lid',
+        type: 'char',
+        length: '2',
+        isNullable: true
+      },
+      {
+        name: 'period_from',
+        type: 'int',
+        length: '11',
+        isNullable: true
+      },
+      {
+        name: 'period_to',
+        type: 'int',
+        length: '11',
+        isNullable: true
+      },
+      {
+        name: 'period_type',
+        type: 'char',
+        length: '1',
+        isNullable: true
+      },
+      {
+        name: 'weight_from',
+        type: 'int',
+        length: '11',
+        isNullable: true
+      },
+      {
+        name: 'weight_to',
+        type: 'int',
+        length: '11',
+        isNullable: true
+      },
+      {
+        name: 'order_price_from',
+        type: 'decimal',
+        length: '18,2',
+        isNullable: true
+      },
+      {
+        name: 'order_price_to',
+        type: 'decimal',
+        length: '18,2',
+        isNullable: true
+      },
+      {
+        name: 'order_currency',
+        type: 'char',
+        length: '3',
+        isNullable: true
+      },
+      {
+        name: 'active',
+        type: 'char',
+        length: '1',
+        isNullable: false
+      },
+      {
+        name: 'price',
+        type: 'decimal',
+        length: '18,2',
+        isNullable: false
+      },
+      {
+        name: 'currency',
+        type: 'char',
+        length: '3',
+        isNullable: true
+      },
+      {
+        name: 'sort',
+        type: 'int',
+        length: '1',
+        isNullable: true
+      },
+      {
+        name: 'description',
+        type: 'varchar',
+        length: '255',
+        isNullable: true
+      },
+      {
+        name: 'regions',
+        type: 'json',
+        isNullable: true
+      }
+    ]
+  });
+
+  private tableForeignKeyForOrder = new TableForeignKey({
+    name: 'fk_sale_order_delivery_id',
+    columnNames: ['delivery_id'],
+    referencedColumnNames: ['id'],
+    referencedTableName: 'b_sale_delivery',
+    onDelete: 'CASCADE'
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(this.saleDeliveryTable);
+    await queryRunner.createForeignKey(
+      'b_sale_order',
+      this.tableForeignKeyForOrder
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable(this.saleDeliveryTable);
+  }
+}
+
+// CREATE TABLE IF NOT EXISTS b_sale_delivery (
+//   ID int(11) NOT NULL auto_increment,
+//   `NAME` varchar(255) NOT NULL,
+//   LID char(2) NOT NULL,
+//   PERIOD_FROM int(11) default NULL,
+//   PERIOD_TO int(11) default NULL,
+//   PERIOD_TYPE char(1) default NULL,
+//   WEIGHT_FROM int(11) default NULL,
+//   WEIGHT_TO int(11) default NULL,
+//   ORDER_PRICE_FROM decimal(18,2) default NULL,
+//   ORDER_PRICE_TO decimal(18,2) default NULL,
+//   ORDER_CURRENCY char(3) default NULL,
+//   ACTIVE char(1) NOT NULL default 'Y',
+//   PRICE decimal(18,2) NOT NULL,
+//   CURRENCY char(3) NOT NULL,
+//   SORT int(11) NOT NULL default '100',
+//   DESCRIPTION varchar(250) default NULL,
+//   PRIMARY KEY  (ID),
+//   KEY IXS_DELIVERY_LID (LID)
+// ) ENGINE=MyISAM  DEFAULT CHARSET=cp1251 AUTO_INCREMENT=4
---
+++ src/database/migrations/1592192439385-InsertSaleDeliveryTable.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InsertSaleDeliveryTable1592192439385
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`
+    INSERT INTO b_sale_delivery (ID, NAME, LID, PERIOD_FROM, PERIOD_TO, PERIOD_TYPE, WEIGHT_FROM, WEIGHT_TO, ORDER_PRICE_FROM, ORDER_PRICE_TO, ORDER_CURRENCY, ACTIVE, PRICE, CURRENCY, SORT, DESCRIPTION, REGIONS) VALUES
+    (1, 'Доставка курьером', 's1', 1, 2, 'D', 0, 0, 0.00, 999.99, 'RUB', 'Y', 200.00, 'RUB', 100, 'Без выходных в удобное для Вас время', '{"values": ["10", "20"]}'),
+    (2, 'Почта России', 's1', 11, 27, 'D', 0, 0, 0.00, 999.99, 'RUB', 'Y', 200.00, 'RUB', 200, 'Доставка возможна ТОЛЬКО после 100% предоплаты.', '{"values": ["30"]}'),
+    (3, 'Бесплатная доставка курьером', 's1', 1, 2, 'D', 0, 0, 1000.00, 0.00, 'RUB', 'Y', 0.00, 'RUB', 300, 'Без выходных в удобное для Вас время', '{"values": ["10", "20"]}'),
+    (4, 'Почта России (Бесплатно!)', 's1', 11, 27, 'D', 0, 0, 1000.00, 0.00, 'RUB', 'Y', 0.00, 'RUB', 400, 'Доставка возможна ТОЛЬКО после 100% предоплаты.', '{"values": ["30"]}');
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.clearTable('b_sale_delivery');
+  }
+}
