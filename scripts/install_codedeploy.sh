6077c098fbb24961c8ff56576e7ca709df0f9c82
Andrey Mashukov <andre.mashukov@gmail.com>
2020-07-09 20:19:07 +0800

    Added pm2

--- scripts/install_codedeploy.sh
+++ scripts/install_codedeploy.sh
@@ -2,7 +2,8 @@
 sudo yum install ruby
 sudo yum install wget
 cd /home/ec2-user
-wget https://aws-codedeploy.s3.eu-central-1.amazonaws.com/latest/install
+wget https://	aws-codedeploy-eu-central-1.s3.eu-central-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
+# sudo service codedeploy-agent start
 # sudo service codedeploy-agent status
