26c3d0f9b4aa38b241c8abf59b1ef2cc3063b0df
Andrey Mashukov <andre.mashukov@gmail.com>
2020-07-28 22:43:56 +0800

    back to console transport

--- src/loaders/winstonLoader.ts
+++ src/loaders/winstonLoader.ts
@@ -2,33 +2,33 @@ import {
   MicroframeworkLoader,
   MicroframeworkSettings
 } from 'microframework-w3tec';
-import { configure } from 'winston';
-// import { configure, format, transports } from 'winston';
-import WinstonCloudWatch from 'winston-cloudwatch';
-// import { env } from '../env';
-import * as AWS from 'aws-sdk';
+// import { configure } from 'winston';
+import { configure, format, transports } from 'winston';
+// import WinstonCloudWatch from 'winston-cloudwatch';
+import { env } from '../env';
+// import * as AWS from 'aws-sdk';
 
-AWS.config.update({
-  accessKeyId: process.env.APP_AWS_KEY_ID,
-  secretAccessKey: process.env.APP_AWS_ACCESS_KEY,
-  region: 'eu-central-1'
-});
-
-// const consoleTransport = new transports.Console({
-//   level: env.log.level,
-//   handleExceptions: true,
-//   format:
-//     env.node !== 'development'
-//       ? format.combine(format.json())
-//       : format.combine(format.colorize(), format.simple())
+// AWS.config.update({
+//   accessKeyId: process.env.APP_AWS_KEY_ID,
+//   secretAccessKey: process.env.APP_AWS_ACCESS_KEY,
+//   region: 'eu-central-1'
 // });
 
-const cloudWatchTransport = new WinstonCloudWatch({
-  cloudWatchLogs: new AWS.CloudWatchLogs(),
-  logGroupName: 'ecommerce-backend',
-  logStreamName: 'ecommerce-backend-stream',
-  retentionInDays: 5
+const consoleTransport = new transports.Console({
+  level: env.log.level,
+  handleExceptions: true,
+  format:
+    env.node !== 'development'
+      ? format.combine(format.json())
+      : format.combine(format.colorize(), format.simple())
 });
+
+// const cloudWatchTransport = new WinstonCloudWatch({
+//   cloudWatchLogs: new AWS.CloudWatchLogs(),
+//   logGroupName: 'ecommerce-backend',
+//   logStreamName: 'ecommerce-backend-stream',
+//   retentionInDays: 5
+// });
 
 export const winstonLoader: MicroframeworkLoader = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -36,7 +36,7 @@ export const winstonLoader: MicroframeworkLoader = (
 ) => {
   configure({
     transports: [
-      cloudWatchTransport
+      consoleTransport
     ]
   });
 };
