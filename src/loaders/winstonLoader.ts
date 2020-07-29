872cf4e589b047890f4a92c361898d2100844e82
Andrey Mashukov <andre.mashukov@gmail.com>
2020-07-29 10:59:10 +0800

    Trying to use both console and cloud watch transports

--- src/loaders/winstonLoader.ts
+++ src/loaders/winstonLoader.ts
@@ -4,15 +4,15 @@ import {
 } from 'microframework-w3tec';
 // import { configure } from 'winston';
 import { configure, format, transports } from 'winston';
-// import WinstonCloudWatch from 'winston-cloudwatch';
+import WinstonCloudWatch from 'winston-cloudwatch';
 import { env } from '../env';
-// import * as AWS from 'aws-sdk';
+import * as AWS from 'aws-sdk';
 
-// AWS.config.update({
-//   accessKeyId: process.env.APP_AWS_KEY_ID,
-//   secretAccessKey: process.env.APP_AWS_ACCESS_KEY,
-//   region: 'eu-central-1'
-// });
+AWS.config.update({
+  accessKeyId: process.env.APP_AWS_KEY_ID,
+  secretAccessKey: process.env.APP_AWS_ACCESS_KEY,
+  region: 'eu-central-1'
+});
 
 const consoleTransport = new transports.Console({
   level: env.log.level,
@@ -23,12 +23,12 @@ const consoleTransport = new transports.Console({
       : format.combine(format.colorize(), format.simple())
 });
 
-// const cloudWatchTransport = new WinstonCloudWatch({
-//   cloudWatchLogs: new AWS.CloudWatchLogs(),
-//   logGroupName: 'ecommerce-backend',
-//   logStreamName: 'ecommerce-backend-stream',
-//   retentionInDays: 5
-// });
+const cloudWatchTransport = new WinstonCloudWatch({
+  cloudWatchLogs: new AWS.CloudWatchLogs(),
+  logGroupName: 'ecommerce-backend',
+  logStreamName: 'ecommerce-backend-stream',
+  retentionInDays: 5
+});
 
 export const winstonLoader: MicroframeworkLoader = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -36,7 +36,8 @@ export const winstonLoader: MicroframeworkLoader = (
 ) => {
   configure({
     transports: [
-      consoleTransport
+      consoleTransport,
+      cloudWatchTransport
     ]
   });
 };
