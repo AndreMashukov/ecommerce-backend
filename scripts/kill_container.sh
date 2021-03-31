ed6554824a8ad4669aee1b49317736e4e3ee6c64
Andrey Mashukov <andre.mashukov@gmail.com>
2021-03-31 10:20:45 +0800

    Update kill_container.sh

--- scripts/kill_container.sh
+++ scripts/kill_container.sh
@@ -2,5 +2,13 @@
 echo "Removinng the running container"
 cd ~
 # sudo docker stop -a
-sudo docker stop $(sudo docker ps -aq)
-sudo docker image rm -f $(sudo docker images -a -q)
+# sudo docker stop $(sudo docker ps -aq)
+# sudo docker image rm -f $(sudo docker images -a -q)
+
+CONTAINER=sudo docker ps -aq
+if [ $CONTAINER ]; then
+   sudo docker stop $CONTAINER
+   sudo docker image rm -f $(sudo docker images -a -q)
+else
+   echo "No running containers"
+fi
