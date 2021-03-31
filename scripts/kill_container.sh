9c2fde145e7483cde42b449321560a844dbaced4
Andrey Mashukov <andre.mashukov@gmail.com>
2021-03-31 10:54:26 +0800

    minor changes

--- scripts/kill_container.sh
+++ scripts/kill_container.sh
@@ -5,16 +5,10 @@ cd ~
 # sudo docker stop $(sudo docker ps -aq)
 # sudo docker image rm -f $(sudo docker images -a -q)
 
-CONTAINER=$(sudo docker ps -aq)
-if [ $CONTAINER ]; then
-   sudo docker stop $(sudo docker ps -aq)
-else
-   echo "No running containers"
+if [ "$(sudo docker ps -aq)" ]; then
+  sudo docker stop $(sudo docker ps -aq)
 fi
 
-IMAGE=$(sudo docker images -a -q)
-if [ $IMAGE ]; then
-   sudo docker image rm -f $(sudo docker images -a -q)
-else
-   echo "No images"
+if [ "$(sudo docker images -a -q)" ]; then
+  sudo docker image rm -f $(sudo docker images -a -q)
 fi
